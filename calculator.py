d2058929448cc801ee031b45f5704105f2eaa5d7
hanwuji99 <hanwuji99@outlook.com>
2017-11-20 20:49:19 +0800

    update

--- calculator.py
+++ calculator.py
@@ -1,198 +1,194 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import sys
-import os.path
 import csv
-import os
-import json
-from multiprocessing import Process, Queue,Value
+import queue
+import configparser
+from getopt import getopt
+from datetime import datetime
+from multiprocessing import Process, Queue
+from collections import namedtuple
+
+
+IncomeTaxQuickLookupItem = namedtuple(
+    'IncomeTaxQuickLookupItem',
+    ['start_point', 'tax_rate', 'quick_subtractor']
+)
+
+INCOME_TAX_START_POINT = 3500
+
+INCOME_TAX_QUICK_LOOKUP_TABLE = [
+    IncomeTaxQuickLookupItem(80000, 0.45, 13505),
+    IncomeTaxQuickLookupItem(55000, 0.35, 5505),
+    IncomeTaxQuickLookupItem(35000, 0.30, 2755),
+    IncomeTaxQuickLookupItem(9000, 0.25, 1005),
+    IncomeTaxQuickLookupItem(4500, 0.2, 555),
+    IncomeTaxQuickLookupItem(1500, 0.1, 105),
+    IncomeTaxQuickLookupItem(0, 0.03, 0)
+]
+
+q_user = Queue()
+q_result = Queue()
+
+
+class Args(object):
+
+    def __init__(self):
+        options, _ = getopt(sys.argv[1:], 'C:c:d:o:')
+        self.options = dict(options)
+        # print('options',self.options)
+
+    def _value_after_option(self, option):
+        value = self.options.get(option)
+        if value is None and option != '-C':
+            print('Parameter Error')
+            exit()
+        return value
+
+    @property
+    def city(self):
+        return self._value_after_option('-C')
+
+    @property
+    def config_path(self):
+        return self._value_after_option('-c')
+
+    @property
+    def userdata_path(self):
+        return self._value_after_option('-d')
+
+    @property
+    def export_path(self):
+        return self._value_after_option('-o')
+
+
+args = Args()
 
 
 class Config(object):
-    def __init__(self, configfile):
-        self._config = {}
-        self.set_config(configfile)
 
-    def set_config(self, filename):
-        if os.path.exists(filename) and filename.endswith('cfg'):
-            try:
-                with open(filename) as file:
-                    for line in file:
-                        line_list = line.strip().split(' = ')
-                        k = line_list[0]
-                        v = line_list[1]
-                        self._config[k] = v
-                    # print('_config',self._config)
-                    return self._config
-            except:
-                print('Parameter Error')
+    def __init__(self):
+        self.config = self._read_config()
+
+    def _read_config(self):
+        config = configparser.ConfigParser()
+        config.read(args.config_path)
+        if args.city and args.city.upper() in config.sections():
+            return config[args.city.upper()]
         else:
-            print("Parameter Error")
+            return config['DEFAULT']
 
-    def get_config(self, k):
-        return self._config[k]
-
-
-class UserData(object):
-    def __init__(self, userdatafile):
-        self.userdata = []
-        self.set_userdata(userdatafile)
-
-    def set_userdata(self, filename):
-        if os.path.exists(filename) and filename.endswith('csv'):
-            try:
-                with open(filename) as file:
-                    line_list = file.readlines()
-                    for line in line_list:
-                        _line = line.strip().split(',')
-                        # print('_line',_line)
-                        self.userdata.append(_line)
-                    # print('userdata', self.userdata)
-                    return self.userdata
-            except:
-                print("Parameter Error")
-        else:
-            print("Parameter Error")
-
-    def get_usersalary(self):
-        return self.userdata
-
-    def calculator(self, user_salary):
+    def _get_config(self, name):
         try:
-            SheBaoRate = float(config.get_config('YangLao')) \
-                         + float(config.get_config('YiLiao')) \
-                         + float(config.get_config('ShiYe')) \
-                         + float(config.get_config('GongShang')) \
-                         + float(config.get_config('ShengYu')) \
-                         + float(config.get_config('GongJiJin'))
-            JiShuL = float(config.get_config('JiShuL'))
-            JiShuH = float(config.get_config('JiShuH'))
-            # print('shebao',SheBaoRate,type(SheBaoRate))
-            # user_salary = self.get_usersalary()
-            # print('user_salary', user_salary)
-            data = []
-            for i in user_salary:
-                salary_info = []
-                salary_info.append(int(i[0]))
-                salary = int(i[1])
-                if salary < JiShuL:
-                    taxable_income = 0
-                    quick_calculation_deduction = 0
-                    tax_rate = 3 / 100
-                    social_security = "{:.2f}".format(JiShuL * SheBaoRate).strip()
-                    # social_security = float(JiShuL * SheBaoRate)
-
-
-                elif salary > JiShuL and salary <= 3500:
-                    taxable_income = 0
-                    quick_calculation_deduction = 0
-                    tax_rate = 3 / 100
-                    social_security = "{:.2f}".format(salary * SheBaoRate).strip()
-                    # social_security = float(salary * SheBaoRate)
-
-                elif salary > 3500 and salary <= JiShuH:
-                    taxable_income = salary - salary * SheBaoRate - 3500
-                    social_security = "{:.2f}".format(salary * SheBaoRate).strip()
-                    # social_security = float(salary * SheBaoRate)
-
-                elif salary > JiShuH:
-                    taxable_income = salary - JiShuH * SheBaoRate - 3500
-                    social_security = "{:.2f}".format(JiShuH * SheBaoRate).strip()
-                    # social_security = float(JiShuH * SheBaoRate)
-
-                if taxable_income <= 1500:
-                    quick_calculation_deduction = 0
-                    tax_rate = 3 / 100
-                elif taxable_income > 1500 and taxable_income <= 4500:
-                    quick_calculation_deduction = 105
-                    tax_rate = 1 / 10
-                elif taxable_income > 4500 and taxable_income <= 9000:
-                    quick_calculation_deduction = 555
-                    tax_rate = 1 / 5
-                elif taxable_income > 9000 and taxable_income <= 35000:
-                    quick_calculation_deduction = 1005
-                    tax_rate = 1 / 4
-                elif taxable_income > 35000 and taxable_income <= 55000:
-                    quick_calculation_deduction = 2755
-                    tax_rate = 3 / 10
-                elif taxable_income > 55000 and taxable_income <= 80000:
-                    quick_calculation_deduction = 5505
-                    tax_rate = 35 / 100
-                elif taxable_income > 80000:
-                    quick_calculation_deduction = 13505
-                    tax_rate = 45 / 100
-                taxable_amount = format((taxable_income * tax_rate - quick_calculation_deduction), ".2f")
-                salary_after_tax = format((salary - float(social_security) - float(taxable_amount)), ".2f")
-                # 税前工资, 社保金额, 个税金额, 税后工资
-                salary_info.append(i[1])
-                salary_info.append(social_security)
-                salary_info.append(taxable_amount)
-                salary_info.append(salary_after_tax)
-                # print('salary_info',salary_info)
-                data.append(salary_info)
-            return data
-        except:
-            print("Parameter Error")
-
-    def dumptofile(self, result, outputfile):
-        # print(result)
-        try:
-            with open(outputfile, 'w') as file:
-                writer = csv.writer(file)
-                writer.writerows(result)
-        except:
+            return float(self.config[name])
+        except (ValueError, KeyError):
             print('Parameter Error')
+            exit()
+
+    @property
+    def social_insurance_baseline_low(self):
+        return self._get_config('JiShuL')
+
+    @property
+    def social_insurance_baseline_high(self):
+        return self._get_config('JiShuH')
+
+    @property
+    def social_insurance_total_rate(self):
+        return sum([
+            self._get_config('YangLao'),
+            self._get_config('YiLiao'),
+            self._get_config('ShiYe'),
+            self._get_config('GongShang'),
+            self._get_config('ShengYu'),
+            self._get_config('GongJiJin')
+        ])
 
 
-queue1 = Queue()
-queue2 = Queue()
+config = Config()
 
 
-#
-def f1():
-    data = userdata.get_usersalary()
-    # print('read',data)
-    # print('process_1:{}'.format(os.getpid()))
-    queue1.put(data)
+class UserData(Process):
+
+    def _read_users_data(self):
+        with open(args.userdata_path) as f:
+            for line in f.readlines():
+                employee_id, income_string = line.strip().split(',')
+                try:
+                    income = int(income_string)
+                except ValueError:
+                    print('Parameter Error')
+                    exit()
+                yield (employee_id, income)
+
+    def run(self):
+        for data in self._read_users_data():
+            # print('data',data)
+            q_user.put(data)
 
 
-#
-def f2():
-    data = queue1.get()
-    # print('getq1data',data,type(data))
-    newdata = userdata.calculator(data)
-    # print('newdata',newdata,type(newdata))
-    queue2.put(newdata)
-    # print('process_2: {}'.format(os.getpid()))
+class IncomeTaxCalculator(Process):
+
+    @staticmethod
+    def calc_social_insurance_money(income):
+        if income < config.social_insurance_baseline_low:
+            return config.social_insurance_baseline_low * \
+                config.social_insurance_total_rate
+        if income > config.social_insurance_baseline_high:
+            return config.social_insurance_baseline_high * \
+                config.social_insurance_total_rate
+        return income * config.social_insurance_total_rate
+
+    @classmethod
+    def calc_income_tax_and_remain(cls, income):
+        social_insurance_money = cls.calc_social_insurance_money(income)
+        real_income = income - social_insurance_money
+        taxable_part = real_income - INCOME_TAX_START_POINT
+        if taxable_part <= 0:
+            return '0.00', '{:.2f}'.format(real_income)
+        for item in INCOME_TAX_QUICK_LOOKUP_TABLE:
+            if taxable_part > item.start_point:
+                tax = taxable_part * item.tax_rate - item.quick_subtractor
+                return '{:.2f}'.format(tax), '{:.2f}'.format(real_income - tax)
+
+    def calc_for_all_userdata(self):
+        while True:
+            try:
+                employee_id, income = q_user.get(timeout=1)
+            except queue.Empty:
+                return
+            data = [employee_id, income]
+            social_insurance_money = '{:.2f}'.format(self.calc_social_insurance_money(income))
+            tax, remain = self.calc_income_tax_and_remain(income)
+            data.extend([social_insurance_money, tax, remain])
+            data.append(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+            # print('yield_data',data)
+            yield data
+
+    def run(self):
+        for data in self.calc_for_all_userdata():
+            # print('data',data)
+            q_result.put(data)
 
 
-def f3():
-    newdata = queue2.get()
-    # print('getq2data',newdata,type(newdata))
-    userdata.dumptofile(newdata,outputfile)
-    # print('process_3: {}'.format(os.getpid()))
+class Exporter(Process):
 
-def main():
-    Process(target=f1).start()
-    Process(target=f2).start()
-    Process(target=f3).start()
+    def run(self):
+        with open(args.export_path, 'w', newline='') as f:
+            while True:
+                writer = csv.writer(f)
+                try:
+                    item = q_result.get(timeout=1)
+                except queue.Empty:
+                    return
+                writer.writerow(item)
 
 
 if __name__ == '__main__':
-    args = sys.argv[1:]
-    if len(args) == 6:
-        index_c = args.index('-c')
-        configfile = args[index_c + 1]
-        # print('configfile',configfile)
-        index_d = args.index('-d')
-        userdatafile = args[index_d + 1]
-        # print('userdatafile',userdatafile)
-        index_o = args.index('-o')
-        outputfile = args[index_o + 1]
-        config = Config(configfile)
-        userdata = UserData(userdatafile)
-
-        main()
-    else:
-        print("Parameter Error")
-        sys.exit(-1)
-    sys.exit(0)
+    workers = [
+        UserData(),
+        IncomeTaxCalculator(),
+        Exporter()
+    ]
+    for worker in workers:
+        worker.run()
